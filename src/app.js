1845e38d7ba68b2c6edfd8858e3c4d069b172626
BeaNovo <168178819+BeaNovo@users.noreply.github.com>
2024-11-30 16:14:54 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,69 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateCard() {
+  let values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let suits = [
+    { name: "hearts", symbol: "♥" },
+    { name: "diamonds", symbol: "♦" },
+    { name: "clubs", symbol: "♣" },
+    { name: "spades", symbol: "♠" }
+  ];
+
+  //seleccionar 1 número y 1 palo de forma aleatoria
+  let value = values[Math.floor(Math.random() * values.length)];
+  let suit = suits[Math.floor(Math.random() * suits.length)];
+
+  // generar carta a partir de número y palo seleccionado
+  let card = document.getElementById("card");
+  card.querySelector(".top").textContent = suit.symbol;
+  card.querySelector(".middle").textContent = value;
+  card.querySelector(".bottom").textContent = suit.symbol;
+
+  // añado al elemento div (id=card) el estilo del palo seleccionado
+  card.className = suit.name;
+}
+
+//reasignar tamaño por usuario
+function resizeCard() {
+  let card = document.getElementById("card");
+  let widthInput = document.getElementById("card-width").value;
+  let heightInput = document.getElementById("card-height").value;
+
+  if (widthInput) {
+    card.style.width = widthInput;
+  }
+  if (heightInput) {
+    card.style.height = heightInput;
+  }
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
+  let el = document.getElementById("generate");
+
+  //reasignación de tamaño de la carta
+  let resizeButton = document.getElementById("resize");
+  resizeButton.addEventListener("click", resizeCard);
+
+  //generador de cartas cada 10 segundos
+  setInterval(generateCard, 10000);
+
+  //botón de generar carta aleatoria
+  el.addEventListener("click", generateCard);
 };
